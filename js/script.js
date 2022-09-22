2becd458afb44f19e1cfec2f1180fcabff8126fd
zee2310 <huzaifamalam2310@gmail.com>
2022-09-22 18:04:10 +0530

    tag added..

--- js/script.js
+++ js/script.js
@@ -1,79 +1,66 @@
-$('.test').on('change', function() {
-    $(".image-text").html(this.value);
-  });
-$('.color-bg').on('change', function() {
-  // console.log(this.value);
-    $(".edit").css('background-color',this.value);
-  });
-$('.font-size').on('change', function() {
-  // console.log(this.value);
-  if(this.value == '' || this.value == '0'){
-    $(".image-text").css("fontSize", "15px");
-  }
-  else{
-    $(".image-text").css("fontSize", this.value+"px");
-  }
-  });
-$('.zee-colors').on('change', function() {
-  // console.log(this.value);
-  if(this.value == ''){
-    $(".image-text").css("fontSize", "15px");
-  }
-  else if(this.value == '1'){
-    $(".image-text").css('color','#00539CFF');
-    $(".edit").css('background-color','#EEA47FFF');
-  }
-  else if(this.value == '2'){
-    $(".image-text").css('color','#EEA47FFF');
-    $(".edit").css('background-color','#00539CFF');
-  }
-  else{
-    $(".image-text").css("fontSize", this.value+"px");
-  }
-  });
-$('.color-text').on('change', function() {
-    $(".image-text").css('color',this.value);
-  });
-$(".color-text").val("#ffffff");
-
-$(".font-family").on('change',function() {
-    //alert($(this).val());
-    $('.image-text').css("font-family", $(this).val());
+$('.test').on('change', function () {
+  $(".image-text").html(this.value);
 });
 
-// $(".imgage-text").css("font-weight", "700");
+$('.color-bg').on('change', function () {
+  // console.log(this.value);
+  $(".edit").css('background-color', this.value);
+});
 
-// document.getElementById("editImg").onclick = function() {
-//   const screenshotTarget = document.getElementById("downloadImg");
+$('.font-size').on('change', function () {
+  // console.log(this.value);
+  if (this.value == '' || this.value == '0') {
+    $(".image-text").css("fontSize", "15px");
+  }
+  else {
+    $(".image-text").css("fontSize", this.value + "px");
+  }
+});
 
-//   html2canvas(screenshotTarget).then((canvas) => {
-//       const base64image = canvas.toDataURL("image/png");
-//       var anchor = document.createElement('a');
-//       anchor.setAttribute("href", base64image);
-//       anchor.setAttribute("download", "image.png");
-//       anchor.click();
-//       anchor.remove();
+$('.zee-colors').on('change', function () {
+  // console.log(this.value);
+  if (this.value == '') {
+    $(".image-text").css("fontSize", "15px");
+  }
+  else if (this.value == '1') {
+    $(".image-text").css('color', '#00539CFF');
+    $(".edit").css('background-color', '#EEA47FFF');
+  }
+  else if (this.value == '2') {
+    $(".image-text").css('color', '#EEA47FFF');
+    $(".edit").css('background-color', '#00539CFF');
+  }
+  else {
+    $(".image-text").css("fontSize", this.value + "px");
+  }
+});
+$('.color-text').on('change', function () {
+  $(".image-text").css('color', this.value);
+});
+$(".color-text").val("#ffffff");
 
-//   })
-// };
+$(".font-family").on('change', function () {
+  //alert($(this).val());
+  $('.image-text').css("font-family", $(this).val());
+});
 
 
-$("#downloadImg").on('click',function() {
+$("#downloadImg").on('click', function () {
   console.log("aaa");
   const screenshotTarget = $("#editImg")[0];
 
   html2canvas(screenshotTarget).then((canvas) => {
     // $("#out_image").append(canvas);
-      const base64image = canvas.toDataURL("jpeg");
-      var anchor = document.createElement('a');
-      anchor.setAttribute("href", base64image);
-      anchor.setAttribute("download", "image.jpeg");
-      anchor.click();
-      anchor.remove();
+    const base64image = canvas.toDataURL("jpeg");
+    var anchor = document.createElement('a');
+    anchor.setAttribute("href", base64image);
+    anchor.setAttribute("download", "image.jpeg");
+    anchor.click();
+    anchor.remove();
 
   })
 });
 
-$(document).on("click","#test-element",function() {
+$(document).on("click", "#test-element", function () {
   alert("click");
 });
