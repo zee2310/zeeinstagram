830b4f1d32f51d12c1a480bcf9237526a1d36cbc
zee2310 <huzaifamalam2310@gmail.com>
2022-09-26 21:12:35 +0530

    color added...

--- js/script.js
+++ js/script.js
@@ -29,26 +29,105 @@ $('.zee-colors').on('change', function () {
   }
   else if (this.value == '1') {
     $(".image-text").css('color', '#00539CFF');
+    $(".author").css('color', '#00539CFF');
+    $(".author-tag").css('color', '#00539CFF');
     $(".edit").css('background-color', '#EEA47FFF');
   }
   else if (this.value == '2') {
     $(".image-text").css('color', '#FBEAEB');
+    $(".author").css('color', '#FBEAEB');
+    $(".author-tag").css('color', '#FBEAEB');
     $(".edit").css('background-color', '#2F3C7E');
   }
   else if (this.value == '3') {
     $(".image-text").css('color', '#2F3C7E');
+    $(".author").css('color', '#2F3C7E');
+    $(".author-tag").css('color', '#2F3C7E');
     $(".edit").css('background-color', '#FBEAEB');
   }
   else if (this.value == '4') {
     $(".image-text").css('color', '#FEE715FF ');
+    $(".author").css('color', '#FEE715FF ');
+    $(".author-tag").css('color', '#FEE715FF ');
     $(".edit").css('background-color', '#101820FF');
   }
   else if (this.value == '5') {
     $(".image-text").css('color', '#101820FF');
+    $(".author").css('color', '#101820FF');
+    $(".author-tag").css('color', '#101820FF');
     $(".edit").css('background-color', '#FEE715FF ');
+  }
+  else if (this.value == '6') {
+    $(".image-text").css('color', '#F96167');
+    $(".author").css('color', '#F96167');
+    $(".author-tag").css('color', '#F96167');
+    $(".edit").css('background-color', '#FCE77D ');
+  }
+  else if (this.value == '7') {
+    $(".image-text").css('color', '#FCE77D');
+    $(".author").css('color', '#FCE77D');
+    $(".author-tag").css('color', '#FCE77D');
+    $(".edit").css('background-color', '#F96167 ');
+  }
+  else if (this.value == '8') {
+    $(".image-text").css('color', '#CCF381');
+    $(".author").css('color', '#CCF381');
+    $(".author-tag").css('color', '#CCF381');
+    $(".edit").css('background-color', '#4831D4 ');
+  }
+  else if (this.value == '9') {
+    $(".image-text").css('color', '#4831D4');
+    $(".author").css('color', '#4831D4');
+    $(".author-tag").css('color', '#4831D4');
+    $(".edit").css('background-color', '#CCF381 ');
+  }
+  else if (this.value == '10') {
+    $(".image-text").css('color', '#317773');
+    $(".author").css('color', '#317773');
+    $(".author-tag").css('color', '#317773');
+    $(".edit").css('background-color', '#E2D1F9 ');
+  }
+  else if (this.value == '11') {
+    $(".image-text").css('color', '#E2D1F9');
+    $(".author").css('color', '#E2D1F9');
+    $(".author-tag").css('color', '#E2D1F9');
+    $(".edit").css('background-color', '#317773 ');
+  }
+  else if (this.value == '12') {
+    $(".image-text").css('color', '#990011FF');
+    $(".author").css('color', '#990011FF');
+    $(".author-tag").css('color', '#990011FF');
+    $(".edit").css('background-color', '#FCF6F5FF ');
+  }
+  else if (this.value == '13') {
+    $(".image-text").css('color', '#FCF6F5FF');
+    $(".author").css('color', '#FCF6F5FF');
+    $(".author-tag").css('color', '#FCF6F5FF');
+    $(".edit").css('background-color', '#990011FF ');
   }
   else {
     $(".image-text").css("fontSize", this.value + "px");
+    $(".author").css("fontSize", this.value + "px");
+    $(".author-tag").css("fontSize", this.value + "px");
+  }
+});
+
+$('.text-align').on('change', function () {
+  // console.log(this.value);
+  if (this.value == '') {
+    $(".image-text").css("fontSize", "15px");
+  }
+  else if (this.value == 'left') {
+    $(".img-text").css('text-align', 'left');
+    // $(".author-text").css('text-align', 'left');
+  }
+  else if (this.value == 'center') {
+    $(".img-text").css('text-align', 'center');
+    // $(".author-text").css('text-align', 'center');
+  }
+  else if (this.value == 'right') {
+    $(".img-text").css('text-align', 'right');
+    // $(".author-text").css('text-align', 'right');
   }
 });
 $('.color-text').on('change', function () {
@@ -59,6 +138,7 @@ $(".color-text").val("#ffffff");
 $(".font-family").on('change', function () {
   //alert($(this).val());
   $('.image-text').css("font-family", $(this).val());
+  $('.author').css("font-family", $(this).val());
   $('.image-text').css("font-weight", '500');
 
 });
